8131c82b5f8985168f7fc2dc5c6a569d7e6d65ef
Thomas Hall <thall1961@gmail.com>
2019-06-08 22:17:31 -0600

    slider and program changes

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,78 +1,110 @@
 import React from "react"
-import { graphql } from "gatsby"
-import Program from './Program'
-
+import PropTypes from "prop-types"
+// import { graphql } from "gatsby"
+import Program from "./Program"
 
 // class component
 class Slider extends React.Component {
-
-    componentDidMount() {
-        const { programs } = programs
-        this.state = {
-            // I'm not sure how to get the programs data from the Index page into the Component and up into state
-            properties: programs,
-            property: programs.index[0]
-        }
-        graphql`
-        query SliderQuery {
-        programs: allPrismicProgram {
-            edges {
-            node {
-                uid
-                data {
-                title {
-                    text
-                }
-                hero_image{
-                    url
-                }
-                lead {
-                    text
-                }
-                }
-            }
-            }
-        }
-        }
-        `
-    }
-  
-    nextProperty = () => {
-      const newIndex = this.state.property.index+1;
-      this.setState({
-        property: properties[newIndex]
-      })
-    }
-  
-    prevProperty = () => {
-      const newIndex = this.state.property.index-1;
-      this.setState({
-        property: properties[newIndex]
-      })
-    }
-  
-    render() {
-      const {property}  = this.state;
-      const {properties} = properties;
-      return (
-        <div className="App">
-  
-          <button 
-            onClick={() => this.nextProperty()} 
-            disabled={property.index === properties.length-1}
-          >Next</button>
-          <button 
-            onClick={() => this.prevProperty()} 
-            disabled={property.index === 0}
-          >Prev</button>
-  
-        {/* Slider  */}
-            <div className="flex flex-col lg:flex-row justify-center items-stretch mt-10">
-                <Program program={ property } />
-            </div>
-        </div>
-      );
-    }
+  state = {
+    index: 0,
   }
-  
-  export default Slider;
+
+  static propTypes = {
+    programs: PropTypes.object,
+  }
+
+  componentDidMount() {
+    // componentDidMount({ data }) {
+    console.log("mounted")
+    // const { programs } = programs
+    // this.setState({
+    //   // I'm not sure how to get the programs data from the Index page into the Component and up into state
+    //   properties: programs,
+    //   property: programs.index[0],
+    // })
+  }
+
+  changeProperty = i => {
+    const index = this.state.index + i
+    this.setState({
+      index,
+    })
+  }
+
+  // nextProperty = () => {
+  //   const newIndex = this.state.property.index + 1
+  //   this.setState({
+  //     property: properties[newIndex],
+  //   })
+  // }
+
+  // prevProperty = () => {
+  //   const newIndex = this.state.property.index - 1
+  //   this.setState({
+  //     property: properties[newIndex],
+  //   })
+  // }
+
+  render() {
+    const index = this.state.index
+    return (
+      <div
+        className="App"
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <button
+          onClick={() => this.changeProperty(-1)}
+          disabled={index === 0}
+          style={{ fontSize: "32px" }}
+        >
+          &larr;
+        </button>
+
+        {/* Slider  */}
+        <div
+          className="flex flex-col lg:flex-row justify-center items-stretch mt-10"
+          style={{ width: "80%" }}
+        >
+          <Program program={this.props.programs[this.state.index]} />
+        </div>
+
+        <button
+          onClick={() => this.changeProperty(1)}
+          disabled={index === this.props.programs.length - 1}
+          style={{ fontSize: "32px" }}
+        >
+          &rarr;
+        </button>
+      </div>
+    )
+  }
+}
+
+// export const query = graphql`
+//   query SliderQuery {
+//     programs: allPrismicProgram {
+//       edges {
+//         node {
+//           uid
+//           data {
+//             title {
+//               text
+//             }
+//             hero_image {
+//               url
+//             }
+//             lead {
+//               text
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
+
+export default Slider
