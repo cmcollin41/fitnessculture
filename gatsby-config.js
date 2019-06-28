e0731917a8be5d319a9102a4433c42cda30282b4
cmcollin41 <cmcollin41@gmail.com>
2019-06-28 15:31:52 -0600

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ require("dotenv").config({
 
 module.exports = {
   plugins: [
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-source-prismic`,
@@ -27,6 +28,28 @@ module.exports = {
       }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Fitness Culture`,
+        short_name: `Fitness Culture`,
+        start_url: `/`,
+        background_color: `#f9f9f9`,
+        theme_color: `#111`,
+        display: `standalone`,
+        icon: `./static/fc-logo-black.png`
+      },
+    },
+    `gatsby-plugin-offline`
   ],
+  siteMetadata: {
+    title: "Fitness Culture | Last Set, Best Set | Join Today",
+    titleTemplate: "%s",
+    description:
+      "Gain muscle, lose weight or improve your explosiveness, it's your world inside of Fitness Culture. Just know you will have thousands of members and coaches there to support you.",
+    url: "https://www.fitnessculture.com", // No trailing slash allowed!
+    image: "./static/fc-logo-horizontal-black.png", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@fitnessculture",
+  }
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,14 @@ import React from "react"
 import Header from './Header'
 import Announcement from './Announcement'
 import Footer from './Footer'
+import SiteMetadata from './SiteMetadata'
 
 import '../css/fonts.css'
 import '../css/global.css'
 
 export default ({ children }) => (
     <div id="layout">
+        <SiteMetadata />
         <Header />
         <Announcement />
         <div>
