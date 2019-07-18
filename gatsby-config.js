66796bfba4928c3364bf4d6c99642348dc4a34ff
Chris Collinsworth <cmcollin41@gmail.com>
2019-07-18 15:33:13 -0600

    removed stripe

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,15 +21,15 @@ module.exports = {
         linkResolver: ({ node, key, value }) => program => `/${program.uid}`,
       },
     },
-    'gatsby-plugin-stripe',
-    {
-      resolve: `gatsby-source-stripe`,
-      options: {
-        objects: ['Product','Sku','Subscription'],
-        secretKey: `${process.env.STRIPE_SECRET_KEY}`,
-        downloadFiles: true,
-      },
-    },
+    // 'gatsby-plugin-stripe',
+    // {
+    //   resolve: `gatsby-source-stripe`,
+    //   options: {
+    //     objects: ['Product','Sku','Subscription'],
+    //     secretKey: `${process.env.STRIPE_SECRET_KEY}`,
+    //     downloadFiles: true,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
