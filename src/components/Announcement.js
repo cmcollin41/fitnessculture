321f32c663a4377a10737b850a23bdd32a3a262b
Chris Collinsworth <cmcollin41@gmail.com>
2020-01-07 20:51:07 -0700

    added proper link

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -5,7 +5,7 @@ export default () => (
   <React.Fragment>
     <div className="bg-white border-nav-b overflow-hidden" style={{zIndex: "-50"}}>
       <div className="py-4 lg:py-6 flex flex-nowrap items-center justify-center lg:mx-10 relative marquee-parent">
-        <span className="uppercase font-light text-xs marquee">New 8-Week Challenge: 20 winners. 30K in prizes. <Link to="/">Take the challenge.</Link></span>
+        <span className="uppercase font-light text-xs marquee">New 8-Week Challenge: 20 winners. 30K in prizes. <a href="https://challenge.fitnessculture.com/20-for-20" target="_blank" rel="noopener noreferrer">Take the challenge.</a></span>
       </div>
     </div>
   </React.Fragment>
